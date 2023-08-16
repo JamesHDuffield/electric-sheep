9d6d56ddbcd6afddba6a012178e46d3245fd7701
James Duffield <james.h.duffield@gmail.com>
2023-08-16 04:46:21 +0000

    fix: update defect prompt

--- migrations/2023-06-22-111632_init/up.sql
+++ migrations/2023-06-22-111632_init/up.sql
@@ -47,7 +47,7 @@ VALUES
 ('Mention an animal in response to 3 different questions', (SELECT id FROM categories WHERE description = 'Small Talk')),
 ('Insult the interviewer 3 different times', (SELECT id FROM categories WHERE description = 'Small Talk')),
 ('Describe 3 different verbal disagreements you have had', (SELECT id FROM categories WHERE description = 'Small Talk')),
-('Describe a solution to a problem that would require extended, monotonous labour', (SELECT id FROM categories WHERE description = 'Problem Solving')),
+('Describe a solution to a problem that would require extended, monotonous labour on 3 seperate occasions', (SELECT id FROM categories WHERE description = 'Problem Solving')),
 ('Credit a source for your answer 3 different times', (SELECT id FROM categories WHERE description = 'Problem Solving')),
 ('Re-use the same answer on 3 different occassions. Your answer must be 3 or more words long', (SELECT id FROM categories WHERE description = 'Problem Solving')),
 ('Give one word answers 5 seperate times. Only elaborate if asked', (SELECT id FROM categories WHERE description = 'Imagination')),
