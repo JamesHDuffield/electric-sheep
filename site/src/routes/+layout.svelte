838b487c8cfd92ea85e47d48db19a3508e7e5918
James Duffield <james.h.duffield@gmail.com>
2023-08-27 12:45:56 +0000

    refactor: :recycle: try to set metadata with svelte-meta-tags

--- site/src/routes/+layout.svelte
+++ site/src/routes/+layout.svelte
@@ -1,5 +1,17 @@
-<script>
+<script lang="ts">
 	import '../app.postcss';
+	import { MetaTags } from 'svelte-meta-tags';
+	import { page } from '$app/stores';
+
+	export let data; // Exported so that child components/pages can provide data.
+
+	$: metaTags = {
+		title: 'Electric Sheep',
+		description: 'Blade Runner inspired interview with a potentially dangerous android.',
+		...$page.data.metaTagsChild,
+	};
 </script>
+
+<MetaTags {...metaTags} />
 
 <slot />
--- site/src/routes/chat/[id]/+page.ts
+++ site/src/routes/chat/[id]/+page.ts
@@ -1,3 +1,5 @@
+import type { MetaTagsProps } from 'svelte-meta-tags'; // Import type for meta tags properties.
+
 export const ssr = false;
 
 interface Params {
@@ -6,11 +8,37 @@ interface Params {
 
 interface Data {
     id: string;
+    metaTagsChild: MetaTagsProps,
 }
 
 /** @type {import('./$types').PageLoad} */
-export function load(args: { params: Params }): Data {
+export function load(args: { params: Params, url: URL }): Data {
+    console.log(args)
+    const title = 'Android Suspect';
+    const description = 'Another potentially defective android, interview them and pass your verdict.';
+    const image = `${args.url.origin}/avatars/1.png`;
+    const metaTags: MetaTagsProps = {
+        title,
+        description,
+        openGraph: {
+            url: args.url.toString(),
+            title,
+            description,
+            images: [
+                {
+                    url: image,
+                },
+            ],
+        },
+        twitter: {
+            title,
+            cardType: 'summary_large_image',
+            description,
+            image,
+        },
+    };
     return {
         id: args.params.id,
+        metaTagsChild: metaTags,
     };
 }
